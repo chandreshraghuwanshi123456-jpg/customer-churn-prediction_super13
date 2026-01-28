f2e98714eac3bb5aad3956e19a9e242229fa2f21
chandreshraghuwanshi123456-jpg <chandreshraghuwanshi123456@gmail.com>
2026-01-28 17:40:04 +0530

    Update ui.py

---
+++ ui.py
@@ -0,0 +1,179 @@
+     UTC     ] Logs for customer-churn-predictionsuper13-fydvbm98ynr2lg2mssfxsa.streamlit.app/
+
+────────────────────────────────────────────────────────────────────────────────────────
+
+[12:06:40] 🖥 Provisioning machine...
+
+[12:06:40] 🎛 Preparing system...
+
+[12:06:40] ⛓ Spinning up manager process...
+
+[12:06:44] 🚀 Starting up repository: 'customer-churn-prediction_super13', branch: 'main', main module: 'ui.py'
+
+[12:06:44] 🐙 Cloning repository...
+
+[12:06:44] 🐙 Cloning into '/mount/src/customer-churn-prediction_super13'...
+
+[12:06:45] 🐙 Cloned repository!
+
+[12:06:45] 🐙 Pulling code changes from Github...
+
+[12:06:45] 📦 Processing dependencies...
+
+
+──────────────────────────────────────── uv ───────────────────────────────────────────
+
+
+Using uv pip install.
+
+Using Python 3.13.11 environment at /home/adminuser/venv
+
+Resolved [2026-01-28 12:06:49.191605] 54 packages[2026-01-28 12:06:49.192010]  [2026-01-28 12:06:49.192378] in 1.88s[2026-01-28 12:06:49.192684] 
+
+Prepared 54 packages in 12.54s
+
+Installed 54 packages in 359ms
+
+ + altair==4.2.2
+
+ + annotated-doc==0.0.4
+
+ + annotated-types==0.7.0
+
+ + anyio==4.12.1
+
+ + attrs==25.4.0
+
+ + blinker==1.9.0
+
+ + cachetools==6.2.6
+
+ + certifi==2026.1.4
+
+ + charset-normalizer==3.4.4
+
+ + click==8.3.1
+
+ + entrypoints==0.4
+
+ + fastapi==0.128.0
+
+ + gitdb==4.0.12
+
+ + gitpython==3.1.46
+
+ + h11==0.16.0
+
+ + idna==3.11
+
+ + jinja2==3.1.6
+
+ + joblib==1.5.3
+
+ + jsonschema==4.26.0
+
+ + jsonschema-specifications==2025.9.1
+
+ + markupsafe==3.0.3
+
+ + numpy==2.4.1
+
+ + packaging==26.0
+
+ + pandas==2.3.3
+
+ + pillow==12.1.0
+
+ + protobuf==6.33.4
+
+ + pyarrow==23.0.0
+
+ + pydantic==2.12.5
+
+ + pydantic-core==2.41.5
+
+ + pydeck==0.9.1
+
+ + pyjwt==2.10.1
+
+ + python-dateutil==2.9.0.post0
+
+ + python-multipart==0.0.22
+
+ + pytz==2025.2
+
+ + referencing==0.37.0
+
+ + requests==2.32.5
+
+ + rpds-py==0.30.0
+
+ + scikit-learn==1.5.2
+
+ + scipy==1.17.0
+
+ + six==1.17.0
+
+ + smmap==5.0.2
+
+ + starlette==0.50.0
+
+ + streamlit==1.53.1
+
+ + tenacity==9.1.2
+
+ + threadpoolctl==3.6.0
+
+ + toml==0.10.2
+
+ + toolz==1.1.0
+
+ + tornado==6.5.4
+
+ + typing-extensions==4.15.0
+
+ + typing-inspection==0.4.2
+
+ + tzdata==2025.3
+
+ + urllib3==2.6.3
+
+ + uvicorn==0.40.0
+
+ + watchdog==6.0.0
+
+Checking if Streamlit is installed
+
+Found Streamlit version 1.53.1 in the environment
+
+Installing rich for an improved exception logging
+
+Using uv pip install.
+
+Using Python 3.13.11 environment at /home/adminuser/venv
+
+Resolved 4 packages in 233ms
+
+Prepared 4 packages in 412ms
+
+Installed 4 packages in 47ms
+
+ + markdown-it-py==4.0.0
+
+ + mdurl==0.1.2
+
+ + pygments==2.19.2
+
+ + rich==14.3.1
+
+
+────────────────────────────────────────────────────────────────────────────────────────
+
+
+[12:07:05] 🐍 Python dependencies were installed from /mount/src/customer-churn-prediction_super13/requirements.txt using uv.
+
+Check if streamlit is installed
+
+Streamlit is already installed
+
+[12:07:06] 📦 Processed dependencies!
